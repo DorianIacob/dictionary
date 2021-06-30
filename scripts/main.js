db3eb925fec0f85b846e93793b0d2cc8487a4efd
DorianIacob <iacob.dorian.gr@gmail.com>
2021-06-30 09:42:46 +0300

    Add project files to the repo
    Add initial version of the project to the repository (html, css and js files).

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+let dictionary = [];
+let addText = document.getElementById("add-text-field");
+let addButton = document.getElementById("add-button");
+let searchText = document.getElementById("search-text-field");
+let searchButton = document.getElementById("search-button");
+
+addButton.onclick = function () {
+	document.getElementById("found-message").textContent = "";
+	document.getElementById("alert-insert-word-1").textContent = "";
+	if (addText.value !== "") {
+		dictionary.push(addText.value);
+	} else {
+		document.getElementById("alert-insert-word-1").textContent = "Insert a word, please!";
+	}
+	addText.value = "";
+}
+
+searchButton.onclick = function () {
+	document.getElementById("found-message").textContent = "";
+	document.getElementById("alert-insert-word-2").textContent = "";
+	if (dictionary.length === 0) {
+		document.getElementById("found-message").textContent = "Sorry, we haven't found \"" +
+		searchText.value + "\" in our Dictionary! :(";
+	} else if (searchText.value !== "") {
+		for (let i = 0; i < dictionary.length; ++i) {
+			if (dictionary[i] === searchText.value) {
+				document.getElementById("found-message").textContent = "Congratulations! We have found \"" +
+				searchText.value + "\" in our Dictionary! :)";
+				break;
+			} else if (i === dictionary.length - 1) {
+				document.getElementById("found-message").textContent = "Sorry, we haven't found \"" +
+				searchText.value + "\" in our Dictionary! :(";
+			}
+		}
+	} else {
+		document.getElementById("alert-insert-word-2").textContent = "Insert a word, please!";
+	}
+	searchText.value = "";
+}
